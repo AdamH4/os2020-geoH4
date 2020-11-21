da0c3da2df829177c483efbdf4725f304e6d2e47
Adam Harnusek <adam.harnusek@gmail.com>
2020-11-21 17:54:39 +0100

    Pamatovy alokator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,14 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; i++){ // vytvorime zamok pre kazdy cpu
+    initlock(&kmem[i].lock, "kmem");
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +58,14 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off(); // zakazem prerusenia
+  uint64 cpuNumber = cpuid(); // zistim ake je aktualne CPU v praci
+  pop_off(); // otvorim dvere pre prerusenia
+
+  acquire(&kmem[cpuNumber].lock); // budem uzamykat iba zamok daneho CPU
+  r->next = kmem[cpuNumber].freelist; // pridam do linked listu daneho CPU
+  kmem[cpuNumber].freelist = r; // pridam do linked listu daneho CPU
+  release(&kmem[cpuNumber].lock); // uvolnim zamok pre dany CPU
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,13 +76,33 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off(); // zakazem prerusenia
+  uint64 cpuNumber = cpuid(); // zistim ake je aktualne CPU v praci
+  pop_off(); // otvorim dvere pre prerusenia
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+  acquire(&kmem[cpuNumber].lock);
+  r = kmem[cpuNumber].freelist; // vyberame si pamat z freelistu
+  if(r) // pozriem ci nieco je nieco vo freeliste
+    kmem[cpuNumber].freelist = r->next; // pridam tam nieco ak je
+  release(&kmem[cpuNumber].lock); // uvolnim zamok
+
+  if (!r) // ak sa nic nenachadza vo freeliste
+  {
+    // tak prebehnem cyklom vsetky CPU a hladam ci ma niaky zo susedov volnu pamat
+    for (int i = 0; i < NCPU; i++)
+    {
+      if ((uint64)i == cpuNumber) // preskocim svoj CPU(samozrejme)
+        continue;
+      acquire(&kmem[i].lock); // zamknem danu oblast
+      r = kmem[i].freelist; // pozrem do susedovho freeListu
+      if (r) // ak ma volno
+      {
+        kmem[i].freelist = r->next; //tak mu to tam lusknem
+        release(&kmem[i].lock); //tak mu to tam lusknem
+        break;
+      }
+      release(&kmem[i].lock); // uvolnim jeho zamok
+    }
+  }
   return (void*)r;
 }
